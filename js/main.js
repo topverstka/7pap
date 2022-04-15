d0d681f2268791830a0827e8235cad645289d56b
finiko-izh <d.novoselov1993@gmail.com>
2022-04-15 12:05:39 +0400

    fixed animation menu

--- js/main.js
+++ js/main.js
@@ -4,21 +4,21 @@ const body = document.querySelector('body');
 
 // Служебные функции
 function find(selector) {
-	return document.querySelector(selector);
+    return document.querySelector(selector);
 }
 
 function findAll(selectors) {
-	return document.querySelectorAll(selectors);
+    return document.querySelectorAll(selectors);
 }
 
 // Удаляет у всех элементов items класс itemClass
-function removeAll(items,itemClass) {   
+function removeAll(items, itemClass) {
     if (typeof items == 'string') {
-      items = document.querySelectorAll(items)
+        items = document.querySelectorAll(items)
     }
     for (let i = 0; i < items.length; i++) {
-      const item = items[i]
-      item.classList.remove(itemClass)
+        const item = items[i]
+        item.classList.remove(itemClass)
     }
 }
 
@@ -28,15 +28,14 @@ function bodyLock(con) {
     } else if (con === false) {
         body.classList.remove('_lock');
     } else if (con === undefined) {
-		if (!body.classList.contains('_lock')) {
-			body.classList.add('_lock');
-		}
-		else {
-			body.classList.remove('_lock')
-		}
-	} else {
-		console.error('Неопределенный аргумент у функции bodyLock()')
-	}
+        if (!body.classList.contains('_lock')) {
+            body.classList.add('_lock');
+        } else {
+            body.classList.remove('_lock')
+        }
+    } else {
+        console.error('Неопределенный аргумент у функции bodyLock()')
+    }
 }
 
 // Мобильное меню
@@ -48,41 +47,39 @@ const modalMobileWrapper = modalMobile.querySelector('.modal-mobile__wrapper');
 const modalMobileMenu = document.querySelector('.modal-mobile__menu');
 const modalMobileFooterMenu = document.querySelector('.modal-mobile__footer-menu');
 
-function slowAnimation() {
-    modalMobileMenu.style.animation = 'opacity_end 1s linear both';
-    modalMobileFooterMenu.style.animation = 'opacity_end 1s linear both';
-    setTimeout(() => {
-        modalMobileMenu.style = null;
-        modalMobileFooterMenu.style = null;
-    }, 1500);
-}
 
 hamburger.addEventListener('click', () => {
     body.classList.toggle('_lock');
 
     if (modalMobile.classList.contains('_show')) {
-        slowAnimation();
+        modalMobile.classList.remove('_show');
+        hamburgerLine.classList.remove('animate');
         setTimeout(() => {
             header.classList.remove('no-fixed');
-            modalMobile.classList.remove('_show');
-            hamburgerLine.classList.remove('animate');
-        }, 1000);
+            modalMobile.classList.add('opacity');
+        }, 350);
+
+        setTimeout(() => {
+            modalMobileWrapper.classList.add('active_wrap')
+        }, 450);
+
     } else {
         header.classList.add('no-fixed');
         modalMobile.classList.add('_show');
         hamburgerLine.classList.add('animate');
+        modalMobile.classList.remove('opacity');
+        modalMobileWrapper.classList.remove('active_wrap');
     }
 
     document.addEventListener('click', (e) => {
         const target = e.target
         if (modalMobile && target.classList.contains('modal-mobile__wrapper')) {
-            slowAnimation();
             setTimeout(() => {
                 header.classList.remove('no-fixed');
                 hamburgerLine.classList.remove('animate');
                 modalMobile.classList.remove('_show')
                 body.classList.remove('_lock');
-            }, 1000)
+            }, 10)
         };
     })
 });
@@ -97,12 +94,12 @@ function desktopSlider() {
         ourServicesSlider = new Swiper(sliderOurServices, {
             slidesPerView: 3,
             spaceBetween: 52,
-        
+
             breakpoints: {
                 1190: {
                     slidesPerView: 3
                 },
-        
+
                 768: {
                     slidesPerView: 2
                 }
@@ -146,12 +143,12 @@ const sliderReviews = find('.slider-reviews__slider');
 
 const reviewsSlider = new Swiper(sliderReviews, {
     pagination: {
-      el: ".slider-reviews__pagination",
-      type: "fraction",
+        el: ".slider-reviews__pagination",
+        type: "fraction",
     },
     navigation: {
-      nextEl: ".slider-reviews__next",
-      prevEl: ".slider-reviews__prev",
+        nextEl: ".slider-reviews__next",
+        prevEl: ".slider-reviews__prev",
     },
 });
 
@@ -216,7 +213,7 @@ if (window.matchMedia('(min-width: 992px)').matches) {
                     left: 0,
                     behavior: 'smooth'
                 });
-                
+
                 flage = false;
                 document.querySelector('.cases__list-preview').classList.remove('_width');
             }
@@ -318,32 +315,31 @@ function sumbitForm() {
         btnSend.classList.add('send-preloader')
 
         e.preventDefault()
-        
+
         let con = validationForm()
 
         if (con === true) {
             const formData = new FormData()
             const action = form.getAttribute('action')
-    
+
             let response = await fetch(action, {
                 method: 'POST',
                 body: formData
             })
-            
+
             // settimeout здесь для того, чтобы показать работу отправки формы. В дальнейшем это нужно убрать
             setTimeout(() => {
                 if (response.ok) {
                     console.log('Successful')
                     form.reset()
-    
+
                     modal.classList.remove('_show')
                     find('#send-done').classList.add('_show')
                     btnSend.classList.remove('send-preloader')
-                }
-                else {
+                } else {
                     console.log('Error')
                     form.reset()
-    
+
                     modal.classList.remove('_show')
                     find('#send-error').classList.add('_show')
                     btnSend.classList.remove('send-preloader')
@@ -357,40 +353,40 @@ function sumbitForm() {
 // Мобильное меню
 // menu()
 function menu() {
-	const burger = find('.burger')
-	const menu = find('.menu');
-	
-	// Высота меню
-	window.addEventListener('resize', () => {
-		const headerHeight = find('.header').clientHeight
+    const burger = find('.burger')
+    const menu = find('.menu');
 
-		if (window.innerWidth <= 768) {
-			menu.style.paddingTop = headerHeight + 'px'
-		}
-		else {
-			menu.style.paddingTop = 0
-		}
-	})
+    // Высота меню
+    window.addEventListener('resize', () => {
+        const headerHeight = find('.header').clientHeight
 
-	burger.addEventListener('click', (e) => {
-		burger.classList.toggle('burger_close')
-		menu.classList.toggle('_show')
-		bodyLock()
-	})
+        if (window.innerWidth <= 768) {
+            menu.style.paddingTop = headerHeight + 'px'
+        } else {
+            menu.style.paddingTop = 0
+        }
+    })
+
+    burger.addEventListener('click', (e) => {
+        burger.classList.toggle('burger_close')
+        menu.classList.toggle('_show')
+        bodyLock()
+    })
 }
 
 // Функции для модальных окон
 modal()
 
-function modal() {    
+function modal() {
     // Открытие модальных окон при клике по кнопке
     openModalWhenClickingOnBtn()
+
     function openModalWhenClickingOnBtn() {
         const btnsOpenModal = document.querySelectorAll('[data-modal-open]');
-    
+
         for (let i = 0; i < btnsOpenModal.length; i++) {
             const btn = btnsOpenModal[i];
-    
+
             btn.addEventListener('click', (e) => {
                 const dataBtn = btn.dataset.modalOpen;
                 const modal = document.querySelector(`#${dataBtn}`)
@@ -403,17 +399,19 @@ function modal() {
 
     // Открытие модального окна, если в url указан его id
     openModalHash()
+
     function openModalHash() {
         if (window.location.hash) {
             const hash = window.location.hash.substring(1)
             const modal = document.querySelector(`.modal#${hash}`)
-    
+
             if (modal) openModal(modal)
         }
     }
 
     // Показываем/убираем модальное окно при изменения хеша в адресной строке
     checkHash()
+
     function checkHash() {
         window.addEventListener('hashchange', e => {
             const hash = window.location.hash
@@ -426,6 +424,7 @@ function modal() {
 
     // Закрытие модального окна при клике по заднему фону
     closeModalWhenClickingOnBg()
+
     function closeModalWhenClickingOnBg() {
         document.addEventListener('click', (e) => {
             const target = e.target
@@ -437,12 +436,13 @@ function modal() {
 
     // Закрытие модальных окон при клике по крестику
     closeModalWhenClickingOnCross()
+
     function closeModalWhenClickingOnCross() {
         const modalElems = document.querySelectorAll('.modal')
         for (let i = 0; i < modalElems.length; i++) {
             const modal = modalElems[i];
             const closeThisModal = modal.querySelector('.modal__close')
-    
+
             closeThisModal.addEventListener('click', () => {
                 closeModal(modal)
             })
@@ -451,11 +451,12 @@ function modal() {
 
     // Закрытие модальных окон при нажатии по клавише ESC
     closeModalWhenClickingOnESC()
+
     function closeModalWhenClickingOnESC() {
         const modalElems = document.querySelectorAll('.modal')
         for (let i = 0; i < modalElems.length; i++) {
             const modal = modalElems[i];
-    
+
             document.addEventListener('keydown', e => {
                 if (e.key === 'Escape') closeModal(modal)
             })
