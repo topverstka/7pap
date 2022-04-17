2e59eeee12ebb4b5e2ccbd5efe05482a6530be3a
Дмитрий Дикк <dmitrydev91@gmail.com>
2022-04-18 00:33:11 +0400

    update our-strength

--- js/main.js
+++ js/main.js
@@ -22,6 +22,7 @@ function removeAll(items, itemClass) {
     }
 }
 
+// Фиксирует скрол у body
 function bodyLock(con) {
     if (con === true) {
         body.classList.add('_lock');
@@ -131,35 +132,36 @@ window.addEventListener('resize', () => {
     desktopSlider();
 });
 
-const $scrollElem = $('.js-horizontal')
+// Горизонтальная прокрутка колесиком мыши
+const $scrollElem = $('.js-horizontal');
 
-const WINDOW_HEIGHT = $(window).height()
-const WINDOW_WIDTH = $(window).width()
+const WINDOW_HEIGHT = $(window).height();
+const WINDOW_WIDTH = $(window).width();
 
-// export
 function horizontalBlocksScroll() {
   if ($scrollElem.length === 0) return;
 
   $scrollElem.each((i, scrollItem) => {
-    $(scrollItem).addClass('horizontal_initialized')
-    const $blocks = $(scrollItem).find('.js-horizontal-block')
-    const $track = $(scrollItem).find('.js-horizontal-track')
+    $(scrollItem).addClass('horizontal_initialized');
+    const $blocks = $(scrollItem).find('.js-horizontal-block');
+    const $track = $(scrollItem).find('.js-horizontal-track');
 
-    const length = $blocks.length
-    const itemWidth = 100 / length
+    const length = $blocks.length;
+    const itemWidth = 100 / length;
 
-    $track.attr('id', `horizontal-track-${i}`)
+    $track.attr('id', `horizontal-track-${i}`);
 
     // Ширина трэка и слайдов
-    $track.css('width', `${100 * length}%`)
-    $blocks.each((i, block) => $(block).css('width', `${itemWidth}%`))
+    $track.css('width', `${100 * length}%`);
+    $blocks.each((i, block) => $(block).css('width', `${itemWidth}%`));
 
-    let wipeAnimation = new TimelineMax()
+    let wipeAnimation = new TimelineMax();
+
     for (let i = 1; i < length; i++) {
-      wipeAnimation.to($track, 0.2, { x: `-${itemWidth * i}%` })
+      wipeAnimation.to($track, 0.2, { x: `-${itemWidth * i}%` });
     }
 
-    const controller = new ScrollMagic.Controller()
+    const controller = new ScrollMagic.Controller();
 
     // Инициализация сцены
     new ScrollMagic.Scene({
@@ -171,7 +173,7 @@ function horizontalBlocksScroll() {
       .setPin(scrollItem)
       .setTween(wipeAnimation)
       .addTo(controller)
-  })
+  });
 }
 
 if (window.matchMedia('(min-width: 993px)').matches) {
@@ -180,26 +182,22 @@ if (window.matchMedia('(min-width: 993px)').matches) {
 
 // Секция Наша сила
 const ourStrengthCounterPlus = find('.our-strength__item.counter--plus');
-const ourStrengthCounterMinus = find('.our-strength__item.counter--minus');
 const ourStrengthItemsHidden = findAll('.our-strength__item.hidden');
 
 if (ourStrengthCounterPlus && ourStrengthItemsHidden) {
     ourStrengthCounterPlus.addEventListener('click', () => {
-        ourStrengthCounterPlus.style.display = 'none';
-        ourStrengthCounterMinus.classList.add('active');
+        ourStrengthCounterPlus.classList.toggle('active');
 
-        ourStrengthCounterMinus.addEventListener('click', () => {
-            ourStrengthCounterMinus.classList.remove('active');
-            ourStrengthCounterPlus.style.display = 'block';
-
+        if (ourStrengthCounterPlus.classList.contains('active')) {
+            ourStrengthItemsHidden.forEach(item => {
+                item.classList.remove('hidden');
+            });
+        } else {
             ourStrengthItemsHidden.forEach(item => {
                 item.classList.add('hidden');
             });
-        });
-
-        ourStrengthItemsHidden.forEach(item => {
-            item.classList.remove('hidden');
-        });
+        }
+        
     });
 }
 
@@ -303,248 +301,3 @@ scrollTop.addEventListener('click', (event) => {
         });
     }
 });
-
-/*
-const swiper = new Swiper('.swiper-container', {
-  
-  slidesPerView: 1, // Кол-во показываемых слайдов
-  spaceBetween: 0, // Расстояние между слайдами
-  loop: true, // Бесконечный слайдер
-  freeMode: true, // Слайдеры не зафиксированны
-  centeredSlides: false, // Размещать слайдеры по центру
-
-  autoplay: { // автопрокрутка
-      delay: 5000, // задержка
-  },
-
-  breakpoints: {
-    1200: {
-
-    },
-    700: {
-
-    },
-    400: {
-
-    }
-  },
-
-  pagination: {
-    el: '.swiper-pagination',
-  },
-
-  navigation: {
-    nextEl: '.swiper__arrow-next',
-    prevEl: '.swiper__arrow-prev',
-  },
-
-  scrollbar: {
-    el: '.swiper-scrollbar',
-  },
-});
-*/
-
-// Валидация формы
-function validationForm() {
-    const name = find('#user_name')
-    const phone = find('#user_phone')
-    const email = find('#user_email')
-
-    let con = true
-
-    for (let i = 0; i < [name, phone, email].length; i++) {
-        const elem = [name, phone, email][i];
-        const elemValue = elem.value.trim()
-
-        if (elemValue === '') {
-            elem.classList.add('_error')
-            con = false
-        } else {
-            elem.classList.remove('_error')
-            con = true
-        }
-    }
-
-    return con
-}
-
-// Отправка формы
-// sumbitForm()
-
-function sumbitForm() {
-    const form = find('.modal__form')
-
-    form.addEventListener('submit', async e => {
-        const modal = find('.modal._show')
-        const btnSend = form.querySelector('[type=submit]')
-        btnSend.classList.add('send-preloader')
-
-        e.preventDefault()
-
-        let con = validationForm()
-
-        if (con === true) {
-            const formData = new FormData()
-            const action = form.getAttribute('action')
-
-            let response = await fetch(action, {
-                method: 'POST',
-                body: formData
-            })
-
-            // settimeout здесь для того, чтобы показать работу отправки формы. В дальнейшем это нужно убрать
-            setTimeout(() => {
-                if (response.ok) {
-                    console.log('Successful')
-                    form.reset()
-
-                    modal.classList.remove('_show')
-                    find('#send-done').classList.add('_show')
-                    btnSend.classList.remove('send-preloader')
-                } else {
-                    console.log('Error')
-                    form.reset()
-
-                    modal.classList.remove('_show')
-                    find('#send-error').classList.add('_show')
-                    btnSend.classList.remove('send-preloader')
-                }
-            }, 2000)
-
-        }
-    })
-}
-
-// Мобильное меню
-// menu()
-function menu() {
-    const burger = find('.burger')
-    const menu = find('.menu');
-
-    // Высота меню
-    window.addEventListener('resize', () => {
-        const headerHeight = find('.header').clientHeight
-
-        if (window.innerWidth <= 768) {
-            menu.style.paddingTop = headerHeight + 'px'
-        } else {
-            menu.style.paddingTop = 0
-        }
-    })
-
-    burger.addEventListener('click', (e) => {
-        burger.classList.toggle('burger_close')
-        menu.classList.toggle('_show')
-        bodyLock()
-    })
-}
-
-// Функции для модальных окон
-modal()
-
-function modal() {
-    // Открытие модальных окон при клике по кнопке
-    openModalWhenClickingOnBtn()
-
-    function openModalWhenClickingOnBtn() {
-        const btnsOpenModal = document.querySelectorAll('[data-modal-open]');
-
-        for (let i = 0; i < btnsOpenModal.length; i++) {
-            const btn = btnsOpenModal[i];
-
-            btn.addEventListener('click', (e) => {
-                const dataBtn = btn.dataset.modalOpen;
-                const modal = document.querySelector(`#${dataBtn}`)
-
-                openModal(modal)
-                window.location.hash = dataBtn
-            });
-        }
-    }
-
-    // Открытие модального окна, если в url указан его id
-    openModalHash()
-
-    function openModalHash() {
-        if (window.location.hash) {
-            const hash = window.location.hash.substring(1)
-            const modal = document.querySelector(`.modal#${hash}`)
-
-            if (modal) openModal(modal)
-        }
-    }
-
-    // Показываем/убираем модальное окно при изменения хеша в адресной строке
-    checkHash()
-
-    function checkHash() {
-        window.addEventListener('hashchange', e => {
-            const hash = window.location.hash
-            const modal = document.querySelector(`.modal${hash}`)
-
-            if (find('.modal._show')) find('.modal._show').classList.remove('_show')
-            if (modal && hash != '') openModal(modal)
-        })
-    }
-
-    // Закрытие модального окна при клике по заднему фону
-    closeModalWhenClickingOnBg()
-
-    function closeModalWhenClickingOnBg() {
-        document.addEventListener('click', (e) => {
-            const target = e.target
-            const modal = document.querySelector('.modal._show')
-
-            if (modal && target.classList.contains('modal__body')) closeModal(modal)
-        })
-    }
-
-    // Закрытие модальных окон при клике по крестику
-    closeModalWhenClickingOnCross()
-
-    function closeModalWhenClickingOnCross() {
-        const modalElems = document.querySelectorAll('.modal')
-        for (let i = 0; i < modalElems.length; i++) {
-            const modal = modalElems[i];
-            const closeThisModal = modal.querySelector('.modal__close')
-
-            closeThisModal.addEventListener('click', () => {
-                closeModal(modal)
-            })
-        }
-    }
-
-    // Закрытие модальных окон при нажатии по клавише ESC
-    closeModalWhenClickingOnESC()
-
-    function closeModalWhenClickingOnESC() {
-        const modalElems = document.querySelectorAll('.modal')
-        for (let i = 0; i < modalElems.length; i++) {
-            const modal = modalElems[i];
-
-            document.addEventListener('keydown', e => {
-                if (e.key === 'Escape') closeModal(modal)
-            })
-        }
-    }
-
-    // Сброс id модального окна в url
-    function resetHash() {
-        const windowTop = window.pageYOffset
-        window.location.hash = ''
-        window.scrollTo(0, windowTop)
-    }
-
-    // Открытие модального окна
-    function openModal(modal) {
-        modal.classList.add('_show')
-        bodyLock(true)
-    }
-
-    // Закрытие модального окна
-    function closeModal(modal) {
-        modal.classList.remove('_show')
-        bodyLock(false)
-        resetHash()
-    }
-}
