58ecea149a5aba71095e3cc8a6daa52d44b815f6
Дмитрий Дикк <dmitrydev91@gmail.com>
2022-04-21 19:33:15 +0400

    update reviews section

--- js/main.js
+++ js/main.js
@@ -210,8 +210,8 @@ const sliderReviews = find('.slider-reviews__slider');
 const sliderReviewsName = document.querySelector('.slider-reviews__name');
 const sliderVideoReviewsLink = document.querySelector('.slider-reviews__link.video-reviews');
 const sliderReviewsLink = document.querySelector('.slider-reviews__link.read-more');
-const sliderReviewsNextBtn = document.querySelector('.slider-reviews__next-mobile');
-const sliderReviewsPrevBtn = document.querySelector('.slider-reviews__prev-mobile');
+// const sliderReviewsNextBtn = document.querySelector('.slider-reviews__next-mobile');
+// const sliderReviewsPrevBtn = document.querySelector('.slider-reviews__prev-mobile');
 
 if (sliderReviews) {
     const reviewsSlider = new Swiper(sliderReviews, {
@@ -251,13 +251,13 @@ if (sliderReviews) {
         }
     });
     
-    sliderReviewsPrevBtn.addEventListener('click', () => {
-        reviewsSlider.slidePrev();
-    });
+    // sliderReviewsPrevBtn.addEventListener('click', () => {
+    //     reviewsSlider.slidePrev();
+    // });
     
-    sliderReviewsNextBtn.addEventListener('click', () => {
-        reviewsSlider.slideNext();
-    });
+    // sliderReviewsNextBtn.addEventListener('click', () => {
+    //     reviewsSlider.slideNext();
+    // });
 }
 
 // Анимация для блока с кейсами при скролле
